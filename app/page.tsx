e9f59e0edf6646d2be44ca3d40cbcf959f2e904b
Олексанр Чумак <cauua@air-oleksanr.lan>
2023-10-21 13:53:40 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
-export default function Home() {
+import Image from 'next/image';
+import styles from './page.module.css';
+import { JSX } from 'react';
+export default function Home():JSX.Element {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          <code className={styles.code}>app/page.tsx</code>
         </p>
         <div>
           <a
@@ -91,5 +91,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
